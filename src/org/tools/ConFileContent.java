548e82d72309ac4f6d96e7083ca0fb433746b272
Junko <499099784@qq.com>
2017-02-28 13:52:27 +0800

    20170228

--- src/org/tools/ConFileContent.java
+++ src/org/tools/ConFileContent.java
@@ -9,83 +9,122 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.UnsupportedEncodingException;
 import java.text.SimpleDateFormat;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class ConFileContent {
-	
-	public static String readToString(String fileName) {  
-        String encoding = "GBK";  
-        File file = new File(fileName);  
-        Long filelength = file.length();  
-        byte[] filecontent = new byte[filelength.intValue()];  
-        try {  
-            FileInputStream in = new FileInputStream(file);  
-            in.read(filecontent);  
-            in.close();  
-        } catch (FileNotFoundException e) {  
-            e.printStackTrace();  
-        } catch (IOException e) {  
-            e.printStackTrace();  
-        }  
-        try {  
-            return new String(filecontent, encoding);  
-        } catch (UnsupportedEncodingException e) {  
-            System.err.println("The OS does not support " + encoding);  
-            e.printStackTrace();  
-            return null;  
-        }  
-    } 
-	
-	
-	public static void writeLog(String str)
-    {
-        try
-        {
-        String path="xml\\M_"+org.tools.GetProperties.getKeyValue("System")+"_"+org.tools.GetProperties.getKeyValue("TableNm")+".xml";
-        File file=new File(path);
-        if(!file.exists())
-            file.createNewFile();
-        FileOutputStream out=new FileOutputStream(file,false); //如果追加方式用true        
-        StringBuffer sb=new StringBuffer();
-        sb.append(str+"\n");
-        out.write(sb.toString().getBytes("GBK"));//注意需要转换对应的字符集
-        out.close();
-        }
-        catch(IOException ex)
-        {
-            System.out.println(ex.getStackTrace());
-        }
-    }    
-	
-	public static void ReplaceColumnNm(String filename){
-		try {  
-            FileInputStream in = new FileInputStream(filename);  
-            InputStreamReader inReader = new InputStreamReader(in, "GBK");  
-            BufferedReader bufReader = new BufferedReader(inReader);  
-            String line = null;  
-            int i = 1;  
-            String reg=".*TARGETFIELD.*";  //判断字符串中是否含有ll
-            while((line = bufReader.readLine()) != null){  
-            	
-            	if(line.matches(reg)){
-                    System.out.println("第" + i + "行：" + line); 
-            	}
-                i++;  
-            }  
-            bufReader.close();  
-            inReader.close();  
-            in.close();  
-        } catch (Exception e) {  
-            e.printStackTrace();  
-            System.out.println("读取" + filename + "出错！");  
-        }  
-		
-	} 
-	
-	public static void main( String args[] ) {
-//		String XmlData = readToString("M_"+org.tools.GetProperties.getKeyValue("System")+"_"+org.tools.GetProperties.getKeyValue("TableNm")+".xml").replace("<ATTRIBUTE NAME=\"Parameter Filename\" VALUE=\"\"/>", "<ATTRIBUTE NAME=\"Parameter Filename\" VALUE=\"$PMRootDir/EDWParam/edw.param\"/>");
-//        System.out.println("<ATTRIBUTE NAME=\"Parameter Filename\" VALUE=\"$PMRootDir/EDWParam/edw.param\"/>");
-//		writeLog(XmlData);
-		ReplaceColumnNm("D:\\workspace\\Uoo\\UpSertMapping.xml");
+
+	public static String readToString(String fileName) {
+		String encoding = "GBK";
+		File file = new File(fileName);
+		Long filelength = file.length();
+		byte[] filecontent = new byte[filelength.intValue()];
+		try {
+			FileInputStream in = new FileInputStream(file);
+			in.read(filecontent);
+			in.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		try {
+			return new String(filecontent, encoding);
+		} catch (UnsupportedEncodingException e) {
+			System.err.println("The OS does not support " + encoding);
+			e.printStackTrace();
+			return null;
+		}
+	}
+
+	public static String writeLog(String str) {
+		try {
+			String path = "xml\\M_" + org.tools.GetProperties.getKeyValue("System") + "_"
+					+ org.tools.GetProperties.getKeyValue("TableNm") + ".xml";
+			File file = new File(path);
+			if (!file.exists())
+				file.createNewFile();
+			FileOutputStream out = new FileOutputStream(file, false); // 如果追加方式用true
+			StringBuffer sb = new StringBuffer();
+			sb.append(str + "\n");
+			out.write(sb.toString().getBytes("GBK"));// 注意需要转换对应的字符集
+			out.close();
+		} catch (IOException ex) {
+			System.out.println(ex.getStackTrace());
+		}
+		return str;
+	}
+
+	public static String ReplaceColumnNm(String filename) {
+		StringBuffer Data = new StringBuffer();
+		try {
+			FileInputStream in = new FileInputStream(filename);
+			InputStreamReader inReader = new InputStreamReader(in, "GBK");
+			BufferedReader bufReader = new BufferedReader(inReader);
+			String line = null;
+
+			String TagReg = ".*TARGETFIELD.*"; // 判断字符串中是否含有TARGETFIELD
+			String Tagregex = ".* NAME=\".*?_out\".*?";
+
+			String ConReg = ".*CONNECTOR.*"; // 判断字符串中是否含有CONNECTOR
+			String Conregex = "TOFIELD=\"(.*?)_out\"";
+			String TransType = ".*FROMINSTANCE=\"updateStrategy_transform\".*";
+			while ((line = bufReader.readLine()) != null) {
+
+				if (line.matches(Tagregex) && line.matches(TagReg)) {
+					Pattern pattern = Pattern.compile(" NAME=\"(.*?)_out\"");
+
+					Matcher m = pattern.matcher(line);
+					if (m.find()) {
+						String ReplaceStr = m.group(1);
+						// System.out.println("第" + i + "行：" + sourceStrArray);
+						System.out.println(line.replaceAll(" NAME=\".*?_out\"", " NAME=\"" + ReplaceStr + "\""));
+						Data.append(line.replaceAll(" NAME=\".*?_out\"", " NAME=\"" + ReplaceStr + "\""));
+						Data.append("\n");
+					}
+
+				} else if (line.matches(ConReg) && line.matches(TransType)) {
+					Pattern pattern = Pattern.compile(Conregex);
+
+					Matcher m1 = pattern.matcher(line);
+					if (m1.find()) {
+						String ReplaceStr = m1.group(1);
+						// System.out.println("第" + i + "行：" + sourceStrArray);
+						// System.out.println(line.replaceAll("TOFIELD=\"(.*?)_out\"",
+						// "TOFIELD=\""+ReplaceStr+"\""));
+						System.out.println("++++++++++++++++++"+ReplaceStr);
+						Data.append(line.replaceAll("TOFIELD=\".*?_out\"", "TOFIELD=\"" + ReplaceStr + "\""));
+						Data.append("\n");
+					}else{
+						Data.append(line + "\n");
+					}
+					// System.out.println("第" + i + "行：" +line);
+
+				} else {
+					Data.append(line + "\n");
+				}
+			}
+			bufReader.close();
+			inReader.close();
+			in.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.out.println("读取" + filename + "出错！");
+		}
+		return Data.toString();
+
+	}
+
+	public static void main(String args[]) {
+		// String XmlData =
+		// readToString("M_"+org.tools.GetProperties.getKeyValue("System")+"_"+org.tools.GetProperties.getKeyValue("TableNm")+".xml").replace("<ATTRIBUTE
+		// NAME=\"Parameter Filename\" VALUE=\"\"/>", "<ATTRIBUTE
+		// NAME=\"Parameter Filename\"
+		// VALUE=\"$PMRootDir/EDWParam/edw.param\"/>");
+		// System.out.println("<ATTRIBUTE NAME=\"Parameter Filename\"
+		// VALUE=\"$PMRootDir/EDWParam/edw.param\"/>");
+		// writeLog(XmlData);
+		writeLog(ReplaceColumnNm("D:\\workspace\\Uoo\\UpSertMapping.xml"));
 	}
 
 }
---
+++ src/org/tools/DataTypeTrans.java
@@ -0,0 +1,50 @@
+package org.tools;
+
+import com.informatica.powercenter.sdk.mapfwk.core.NativeDataTypes;
+
+public class DataTypeTrans {
+	
+	public static String Trans(String DataType, String DbType){
+		String sb = null;
+		
+		
+		if(DbType == "Oracle"){
+			switch(DataType.toString().substring(0, DataType.toString().indexOf("(")))
+	        {
+	        case "VARCHAR2": sb = NativeDataTypes.Oracle.VARCHAR2; break;
+	        case "NUMBER": sb = NativeDataTypes.Oracle.NUMBER_PS; break;
+	        case "DATE": sb = NativeDataTypes.Oracle.DATE; break;
+	        case "BLOB": sb = NativeDataTypes.Oracle.BLOB; break;
+	        case "CHAR": sb = NativeDataTypes.Oracle.CHAR; break;
+	        case "CLOB": sb = NativeDataTypes.Oracle.CLOB; break;
+	        case "LONG": sb = NativeDataTypes.Oracle.LONG; break;
+	        case "LONGRAW": sb = NativeDataTypes.Oracle.LONGRAW; break;
+	        case "NCHAR": sb = NativeDataTypes.Oracle.NCHAR; break;
+	        case "NCLOB": sb = NativeDataTypes.Oracle.NCLOB; break;
+	        case "TIMESTAMP": sb = NativeDataTypes.Oracle.TIMESTAMP; break;
+	        case "VARCHAR": sb = NativeDataTypes.Oracle.VARCHAR; break;
+	        default: sb = NativeDataTypes.Oracle.VARCHAR2; break; 
+		
+        }; 
+		}else if(DbType == "TD"){
+			switch(DataType.toString().substring(0, DataType.toString().indexOf("(")))
+	        {
+	        case "VARCHAR2": sb = NativeDataTypes.Teradata.VARCHAR; break;
+	        case "NUMBER": sb = NativeDataTypes.Teradata.DECIMAL; break;
+	        case "DATE": sb = NativeDataTypes.Teradata.TIMESTAMP; break;
+	        case "CHAR": sb = NativeDataTypes.Teradata.CHAR; break;
+	        case "NCHAR": sb = NativeDataTypes.Teradata.CHAR; break;
+	        case "TIMESTAMP": sb = NativeDataTypes.Teradata.TIMESTAMP; break;
+	        case "VARCHAR": sb = NativeDataTypes.Teradata.VARCHAR; break;
+	        default: sb = NativeDataTypes.Teradata.VARCHAR; break; 
+	        };
+		}
+		return sb;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
